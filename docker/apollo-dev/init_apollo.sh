7a609cf2a1a76af4ab16bd2e64d23ebb35da9ddf
Jay Zhang <wangyoucao577@gmail.com>
2020-12-29 03:44:55 +0000

    feat: prepare other data

--- docker/apollo-dev/init_apollo.sh
+++ docker/apollo-dev/init_apollo.sh
@@ -27,6 +27,42 @@ function prepare_maps() {
     cd ${APOLLO_ROOT_DIR}
 }
 
+function prepare_other_data() {
+
+    local TARGET_ARCH=$(uname -m)   # x86_64
+    
+    # AUDIO
+    local AUDIO_DATA_PATH="${APOLLO_ROOT_DIR}/modules/audio/data"
+    local AUDIO_DATA_IMAGE="${APOLLO_DOCKER_REPO}:data_volume-audio_model-${TARGET_ARCH}-${VOLUME_VERSION}"
+    docker pull ${AUDIO_DATA_IMAGE} 
+    mkdir -p ${AUDIO_DATA_PATH}
+    cd ${AUDIO_DATA_PATH}
+    docker run --rm --mount "src=$(pwd),dst=/data,type=bind" ${AUDIO_DATA_IMAGE} "cp ${AUDIO_DATA_PATH}/* /data/ && chmod 777 /data/*"
+    ls -lh 
+    cd ${APOLLO_ROOT_DIR}
+
+    # YOLOV4
+    local YOLOV4_DATA_PATH="${APOLLO_ROOT_DIR}/modules/perception/camera/lib/obstacle/detector/yolov4/model"
+    local YOLOV4_DATA_IMAGE="${APOLLO_DOCKER_REPO}:yolov4_volume-emergency_detection_model-${TARGET_ARCH}-${VOLUME_VERSION}"
+    docker pull ${YOLOV4_DATA_IMAGE} 
+    mkdir -p ${YOLOV4_DATA_PATH}
+    cd ${YOLOV4_DATA_PATH}
+    docker run --rm --mount "src=$(pwd),dst=/data,type=bind" ${YOLOV4_DATA_IMAGE} "cp ${YOLOV4_DATA_PATH}/* /data/ && chmod 777 /data/*"
+    ls -lh 
+    cd ${APOLLO_ROOT_DIR}
+
+    # FASTER_RCNN
+    local FASTER_RCNN_DATA_PATH="${APOLLO_ROOT_DIR}/modules/perception/production/data/perception/camera/models/traffic_light_detection/faster_rcnn_model"
+    local FASTER_RCNN_DATA_IMAGE="${APOLLO_DOCKER_REPO}:faster_rcnn_volume-traffic_light_detection_model-${TARGET_ARCH}-${VOLUME_VERSION}"
+    docker pull ${FASTER_RCNN_DATA_IMAGE} 
+    mkdir -p ${FASTER_RCNN_DATA_PATH}
+    cd ${FASTER_RCNN_DATA_PATH}
+    docker run --rm --mount "src=$(pwd),dst=/data,type=bind" ${FASTER_RCNN_DATA_IMAGE} "cp ${FASTER_RCNN_DATA_PATH}/* /data/ && chmod 777 /data/*"
+    ls -lh 
+    cd ${APOLLO_ROOT_DIR}
+
+}
+
 function clone_code() {
 
     if [ -d ${APOLLO_ROOT_DIR} ]; then
@@ -46,6 +82,8 @@ function main() {
     clone_code
 
     prepare_maps
+
+    prepare_other_data
 
     build_code_cpu
 }
