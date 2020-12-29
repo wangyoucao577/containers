8952fb685b50911d413e7a3b2fb0aa47573ae208
Jay Zhang <wangyoucao577@gmail.com>
2020-12-29 03:17:37 +0000

    feat: export map data from data volume

--- docker/apollo-dev/init_apollo.sh
+++ docker/apollo-dev/init_apollo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -xe
 
 APOLLO_ROOT_DIR="/apollo"
+APOLLO_MAP_DATA_PATH="${APOLLO_ROOT_DIR}/modules/map/data"
+
+APOLLO_DOCKER_REPO="apolloauto/apollo"
 
 # https://github.com/ApolloAuto/apollo/blob/a36d6c5a88a02a2193a7de834413a649d606887a/docker/scripts/dev_start.sh#L57
 DEFAULT_MAPS=(
@@ -12,12 +15,16 @@ DEFAULT_MAPS=(
 VOLUME_VERSION="latest"
 
 function prepare_maps() {
+    cd ${APOLLO_MAP_DATA_PATH}
+
     for map_name in ${DEFAULT_MAPS[@]}; do
-        map_image="apolloauto/apollo:map_volume-${map_name}-${VOLUME_VERSION}"
+        map_image="${APOLLO_DOCKER_REPO}:map_volume-${map_name}-${VOLUME_VERSION}"
         docker pull ${map_image}
-        #TODO: export mapdata 
-        #docker run ${map_image}
+        docker run --rm --mount "src=$(pwd),dst=/mapdata,type=bind" ${map_image} sh -c "cp -r ${APOLLO_MAP_DATA_PATH}/${map_name} /mapdata/ && chmod 777 /mapdata/${map_name}"
     done
+
+    ls -lh 
+    cd ${APOLLO_ROOT_DIR}
 }
 
 function clone_code() {
