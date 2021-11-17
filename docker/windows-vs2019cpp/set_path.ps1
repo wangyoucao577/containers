2ee7667ff306aa9478a959514dd725afd4c0f1b0
Jay Zhang <wangyoucao577@gmail.com>
2021-11-17 22:59:13 +0800

    feat: unify cmake name

--- docker/windows-vs2019cpp/set_path.ps1
+++ docker/windows-vs2019cpp/set_path.ps1
@@ -1,3 +1,3 @@
-[Environment]::SetEnvironmentvariable("PATH", $ENV:PATH + ";C:\Program Files\7-Zip;C:\workspace\ninja-win;C:\workspace\cmake-3.22.0-rc2-windows-x86_64\bin", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentvariable("PATH", $ENV:PATH + ";C:\Program Files\7-Zip;C:\workspace\ninja-win;C:\workspace\cmake-windows-x86_64\bin", [EnvironmentVariableTarget]::Machine)
 [environment]::SetEnvironmentvariable("VS2019_INSTALL_PATH", "C:\Program Files (x86)\Microsoft Visual studio\2019\BuildTools", "Machine")
 [environment]::SetEnvironmentvariable("VCPKG_CMAKE_PATH", "C:\workspace\vcpkg\scripts\buildsystems\vcpkg.cmake", "Machine")
